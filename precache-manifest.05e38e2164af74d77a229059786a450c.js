2e21a07784d6dfbb3056bcb6bec40ab5ac8a8f1e
Vishal Thakur <vishalthakur054@gmail.com>
2020-04-19 03:56:54 +0800

    Updates

---
+++ precache-manifest.05e38e2164af74d77a229059786a450c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af61ac317fd74bd63fd3750b3f9d3ac8",
+    "url": "/corona-tracker-app/index.html"
+  },
+  {
+    "revision": "0b24903a300473458bc8",
+    "url": "/corona-tracker-app/static/css/main.784a5726.chunk.css"
+  },
+  {
+    "revision": "5b511221bd8dada39e1a",
+    "url": "/corona-tracker-app/static/js/2.855ce927.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker-app/static/js/2.855ce927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b24903a300473458bc8",
+    "url": "/corona-tracker-app/static/js/main.35900eb2.chunk.js"
+  },
+  {
+    "revision": "07cdceb4c3ba53ac66af",
+    "url": "/corona-tracker-app/static/js/runtime-main.28c8ecb5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker-app/static/media/corona.d7265326.png"
+  }
+]);
